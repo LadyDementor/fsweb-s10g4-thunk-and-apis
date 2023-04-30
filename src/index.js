cf05324444848cc56a2ed9f918fb1ff35441660c
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-04-30 22:39:44 +0300

    s10g4 proje tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,40 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import {
-  legacy_createStore as createStore,
-  applyMiddleware,
-  compose,
-} from "redux";
-
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+
 import { myReducer } from "./reducers";
 import thunk from "redux-thunk";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-import logger from "redux-logger";
-import { composeWithDevTools } from "@redux-devtools/extension";
+const depo = createStore(myReducer, applyMiddleware(thunk));
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const depo = createStore(
-  myReducer,
-  composeEnhancers(applyMiddleware(thunk, logger))
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-ReactDOM.render(
+root.render(
   <Provider store={depo}>
     <BrowserRouter>
       <>
-        <ToastContainer />
         <App />
       </>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/reducers.js
+++ src/reducers.js
@@ -6,42 +6,52 @@ import {
   FETCH_ERROR,
   GET_FAVS_FROM_LS,
 } from "./actions";
-
 const initial = {
   favs: [],
   current: null,
   error: null,
   loading: true,
 };
-
 function writeFavsToLocalStorage(state) {
   localStorage.setItem("s10g4", JSON.stringify(state.favs));
 }
-
 function readFavsFromLocalStorage() {
   return JSON.parse(localStorage.getItem("s10g4"));
 }
-
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+      return {
+        ...state,
+        favs: [...state.favs, state.current],
+      };
 
     case FAV_REMOVE:
-      return state;
+      return {
+        ...state,
+        favs: state.favs.filter((item) => item.id !== action.payload),
+      };
 
     case FETCH_SUCCESS:
-      return state;
+      return {
+        ...state,
+        current: action.payload,
+      };
 
     case FETCH_LOADING:
-      return state;
+      return {
+        ...state,
+        loading: action.payload,
+      };
 
     case FETCH_ERROR:
-      return state;
+      return {
+        ...state,
+        error: action.payload,
+      };
 
     case GET_FAVS_FROM_LS:
       return state;
-
     default:
       return state;
   }
