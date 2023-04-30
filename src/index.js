31c5ad5ab6fe430c7522ccfb1ca0db9eb5525f7a
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-04-30 21:57:04 +0300

    ilk kurulum tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+
 import { Provider } from "react-redux";
-import { myReducer } from './reducers';
+import { myReducer } from "./reducers";
+import thunk from "redux-thunk";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const depo = createStore(myReducer);
+import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const depo = createStore(
+  myReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactDOM.render(
   <Provider store={depo}>
     <BrowserRouter>
       <>
+        <ToastContainer />
         <App />
       </>
     </BrowserRouter>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
